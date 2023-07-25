defbc856af68d56bdd762c7d3d770c13cc070d2e
erincunningham7 <101681089+erincunningham7@users.noreply.github.com>
2023-07-25 11:49:46 +0000

    Build add turn function

--- scripts/game.js
+++ scripts/game.js
@@ -12,8 +12,14 @@ function newGame() {
     showScore();
 }
 
+function addTurn() {
+    game.playerMoves = [];
+    game.currentGame.push(game.choices[(Math.floor(Math.random() * 4))]);
+    // showTurns();
+}
+
 function showScore() {
     document.getElementById("score").innerText = game.score;
 }
 
-module.exports = { game, newGame, showScore };
+module.exports = { game, newGame, showScore, addTurn, lightsOn };
