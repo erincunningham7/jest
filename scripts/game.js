47f9714d8d9e976f1cadaba7f29458549c4f4b44
erincunningham7 <101681089+erincunningham7@users.noreply.github.com>
2023-07-25 11:52:07 +0000

    add interactivity

--- scripts/game.js
+++ scripts/game.js
@@ -10,12 +10,20 @@ function newGame() {
     game.playerMoves = [];
     game.score = 0;
     showScore();
+    addTurn();
 }
 
 function addTurn() {
     game.playerMoves = [];
     game.currentGame.push(game.choices[(Math.floor(Math.random() * 4))]);
     // showTurns();
+}
+
+function lightsOn(circ) {
+    document.getElementById(circ).classList.add(circ + "light");
+    setTimeout(function () {
+        document.getElementById(circ).classList.remove(circ + "light");
+    }, 400);
 }
 
 function showScore() {
